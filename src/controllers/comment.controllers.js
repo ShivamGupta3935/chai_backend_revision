c5373e1958340e8c85158a8d1a9d181710ada718
shivam gupta <guptashivam3935@gmail.com>
2024-11-14 11:12:13 +0530

    adding all controller related to commnet

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -95,22 +95,99 @@ const getVideoComments = promiseHandler(async (req, res) => {
             "comment fetched successfully"
         )
     )
-
-
-
-
 })
 
 const addComment = promiseHandler(async (req, res) => {
     // TODO: add a comment to a video
+    const {content} = req.body
+    const {videoId} = req.params
+
+    if (!content) {
+        throw new ApiError(400, "content is empty")
+    }
+
+    const _video = await Video.findById(videoId)
+    if (!_video) {
+        throw new ApiError(400, "there are no video this id")
+    }
+
+    const createComment = await Comment.create({
+        content,
+        video: videoId,
+        owner: req..user?._id
+    })
+
+    if (!createComment) {
+        throw new ApiError(500, "fail to add comment plz try again")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, createComment, "comment add successfully")
+    )
+
+
 })
 
 const updateComment = promiseHandler(async (req, res) => {
     // TODO: update a comment
+const {commentId} = req.params
+    const {content} = req.params
+
+    if (!content) {
+        throw new ApiError(400, "no comment added")
+    }
+
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(400, "comment not exist")
+    }
+
+    if (comment?.owner.toString() != req?.user._id.toString()) {
+        throw new ApiError(400, "only comment owene can update this comment ")
+    }
+
+    const updateComment = await Comment.findByIdAndUpdate(
+        comment?._id,
+        {
+            $set: {
+                content
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, updateComment, "comment updated successfully"
+        )
+    )
+
 })
 
 const deleteComment = promiseHandler(async (req, res) => {
     // TODO: delete a comment
+    const {commentId} = req.params
+
+    const comment = await Comment.findById(commentId)
+    if (!comment) {
+        throw new ApiError(400, "no comment exist, invalid comment id ")
+    }
+
+    if (comment.owner.toString() != req?.user._id.toString()){
+        throw new ApiError(400, "only comment owner can delete this commnet")
+    }
+
+    await Comment.findByIdAndDelete(comment._id)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {},"comment deleted successfully"))
 })
 
 export {
